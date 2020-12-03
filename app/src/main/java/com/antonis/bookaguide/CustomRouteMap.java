78f4d155d318f6359107c2925e95575406658e80
antonis-adraktas <antonis.adraktas@gmail.com>
2020-12-03 17:15:26 +0200

    Added a map activity to be accessed from routesFragment

---
+++ app/src/main/java/com/antonis/bookaguide/CustomRouteMap.java
@@ -0,0 +1,43 @@
+package com.antonis.bookaguide;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class CustomRouteMap extends AppCompatActivity
+        implements OnMapReadyCallback {
+
+    // [START_EXCLUDE]
+    // [START maps_marker_get_map_async]
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Retrieve the content view that renders the map.
+        setContentView(R.layout.map_layout);
+
+        // Get the SupportMapFragment and request notification when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        LatLng sydney = new LatLng(38.054708, 22.6785815);
+        googleMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Marker in Melissi"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+    }
+}
+
--- app/src/main/java/com/antonis/bookaguide/tabView/RoutesFragment.java
+++ app/src/main/java/com/antonis/bookaguide/tabView/RoutesFragment.java
@@ -1,10 +1,12 @@
 package com.antonis.bookaguide.tabView;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
 
@@ -12,6 +14,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.antonis.bookaguide.CustomRouteMap;
 import com.antonis.bookaguide.R;
 
 import java.util.Calendar;
@@ -20,6 +23,7 @@ public class RoutesFragment extends Fragment {
 
     private String selectedDate;
     private TextView selectDateTextView;
+    private Button customButton;
 
     public RoutesFragment() {
     }
@@ -42,6 +46,14 @@ public class RoutesFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view =inflater.inflate(R.layout.routes_layout,container,false);
         selectDateTextView=view.findViewById(R.id.selectDateRoutes);
+        customButton=view.findViewById(R.id.button);
+        customButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getActivity(), CustomRouteMap.class);
+                startActivity(intent);
+            }
+        });
         selectDateTextView.setOnClickListener(new ClickListener());
         return view;
     }

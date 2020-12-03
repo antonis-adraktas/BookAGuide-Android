a3e78dc9ec0c3bf9423df54a9706f8f052b46c5c
antonis-adraktas <antonis.adraktas@gmail.com>
2020-12-03 18:22:43 +0200

    Added onClickListener in Map and alert dialog to add a marker

--- app/src/main/java/com/antonis/bookaguide/CustomRouteMap.java
+++ app/src/main/java/com/antonis/bookaguide/CustomRouteMap.java
@@ -1,14 +1,16 @@
 package com.antonis.bookaguide;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class CustomRouteMap extends AppCompatActivity
@@ -32,12 +34,39 @@ public class CustomRouteMap extends AppCompatActivity
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
-        LatLng sydney = new LatLng(38.054708, 22.6785815);
-        googleMap.addMarker(new MarkerOptions()
-                .position(sydney)
-                .title("Marker in Melissi"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLngBounds atticaBounds = new LatLngBounds(
+                new LatLng(37.882943, 23.657002), // SW bounds
+                new LatLng(38.036468, 23.904720)  // NE bounds
+        );
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(atticaBounds, 10));
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(atticaBounds.getCenter(), 10));
+        googleMap.setLatLngBoundsForCameraTarget(atticaBounds);
+        googleMap.setMinZoomPreference(11);
+
+        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                new AlertDialog.Builder(CustomRouteMap.this)
+                        .setMessage(R.string.addMarkerQuestion)
+                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                googleMap.addMarker(new MarkerOptions()
+                                        .position(latLng)
+                                        .title("I add this place to my route."));
+                            }
+                        })
+                        .setNegativeButton(android.R.string.cancel,null)
+                        .setIcon(android.R.drawable.ic_dialog_map)
+                        .show();
+            }
+        });
+
+
 
     }
+
 }
+
+
 

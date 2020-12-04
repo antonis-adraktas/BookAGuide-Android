25e5230420f18a84f27a01b673d62ad67398cf0b
antonis-adraktas <antonis.adraktas@gmail.com>
2020-12-04 15:51:14 +0200

    Added markerList and limit of up to 8 places per trip

--- app/src/main/java/com/antonis/bookaguide/CustomRouteMap.java
+++ app/src/main/java/com/antonis/bookaguide/CustomRouteMap.java
@@ -3,6 +3,7 @@ package com.antonis.bookaguide;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,11 +12,15 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
 
 public class CustomRouteMap extends AppCompatActivity
         implements OnMapReadyCallback {
-
+    private int numMarker=0;
+    private ArrayList<Marker> markerList;
     // [START_EXCLUDE]
     // [START maps_marker_get_map_async]
     @Override
@@ -33,7 +38,7 @@ public class CustomRouteMap extends AppCompatActivity
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
+        markerList=new ArrayList<>();
         LatLngBounds atticaBounds = new LatLngBounds(
                 new LatLng(37.882943, 23.657002), // SW bounds
                 new LatLng(38.036468, 23.904720)  // NE bounds
@@ -43,28 +48,58 @@ public class CustomRouteMap extends AppCompatActivity
         googleMap.setLatLngBoundsForCameraTarget(atticaBounds);
         googleMap.setMinZoomPreference(11);
 
+
         googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(LatLng latLng) {
-                new AlertDialog.Builder(CustomRouteMap.this)
-                        .setMessage(R.string.addMarkerQuestion)
-                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                googleMap.addMarker(new MarkerOptions()
-                                        .position(latLng)
-                                        .title("I add this place to my route."));
-                            }
-                        })
-                        .setNegativeButton(android.R.string.cancel,null)
-                        .setIcon(android.R.drawable.ic_dialog_map)
-                        .show();
+                if (numMarker<8) {
+                    new AlertDialog.Builder(CustomRouteMap.this)         //Alert dialog to add a marker
+                            .setMessage(R.string.addMarkerQuestion)
+                            .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Marker marker;
+                                    numMarker++;
+                                    marker = googleMap.addMarker(new MarkerOptions()                   //marker is added to the map and to arraylist
+                                            .position(latLng)
+                                            .title("This is marker number " + numMarker + " in my route.")
+                                            .draggable(true));
+                                    markerList.add(marker);
+                                    Log.d(MainActivity.LOGAPP, "A marker was added to the list. The position of the last marker added is "
+                                            + markerList.get(markerList.size() - 1).getPosition().toString());
+                                }
+                            })
+                            .setNegativeButton(android.R.string.cancel, null)
+                            .setIcon(android.R.drawable.ic_dialog_map)
+                            .show();
+                }else{
+                    new AlertDialog.Builder(CustomRouteMap.this)             //can add up to 8 places
+                            .setMessage(R.string.markerLimit)
+                            .setPositiveButton(android.R.string.ok,null)
+                            .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {             //map is cleared from markers and user can start over
+                                    googleMap.clear();
+                                    numMarker=0;
+                                    markerList.clear();
+                                }
+                            })
+                            .setIcon(android.R.drawable.ic_dialog_info)
+                            .show();
+                }
             }
         });
 
-
-
     }
+
+//    private void addMarkerDialog(String message){
+//
+//    }
+//    private void mapOnClicklistenerAction implements DialogInterface.OnClickListener{
+//        @Override
+//        public void onClick(DialogInterface dialog, int which) {
+//            Marker marker;
+//    }
 
 }
 

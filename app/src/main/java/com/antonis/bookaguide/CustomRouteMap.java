09b1ef5bcc570cfd8b7e0e407e699010a24c2dcd
antonis-adraktas <antonis.adraktas@gmail.com>
2020-12-04 16:04:14 +0200

    Added functions instead of anonymous classes for Dialogs

--- app/src/main/java/com/antonis/bookaguide/CustomRouteMap.java
+++ app/src/main/java/com/antonis/bookaguide/CustomRouteMap.java
@@ -53,53 +53,52 @@ public class CustomRouteMap extends AppCompatActivity
             @Override
             public void onMapClick(LatLng latLng) {
                 if (numMarker<8) {
-                    new AlertDialog.Builder(CustomRouteMap.this)         //Alert dialog to add a marker
-                            .setMessage(R.string.addMarkerQuestion)
-                            .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    Marker marker;
-                                    numMarker++;
-                                    marker = googleMap.addMarker(new MarkerOptions()                   //marker is added to the map and to arraylist
-                                            .position(latLng)
-                                            .title("This is marker number " + numMarker + " in my route.")
-                                            .draggable(true));
-                                    markerList.add(marker);
-                                    Log.d(MainActivity.LOGAPP, "A marker was added to the list. The position of the last marker added is "
-                                            + markerList.get(markerList.size() - 1).getPosition().toString());
-                                }
-                            })
-                            .setNegativeButton(android.R.string.cancel, null)
-                            .setIcon(android.R.drawable.ic_dialog_map)
-                            .show();
+                    addMarkerWithDialog(CustomRouteMap.this.getString(R.string.addMarkerQuestion),googleMap,latLng);
                 }else{
-                    new AlertDialog.Builder(CustomRouteMap.this)             //can add up to 8 places
-                            .setMessage(R.string.markerLimit)
-                            .setPositiveButton(android.R.string.ok,null)
-                            .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {             //map is cleared from markers and user can start over
-                                    googleMap.clear();
-                                    numMarker=0;
-                                    markerList.clear();
-                                }
-                            })
-                            .setIcon(android.R.drawable.ic_dialog_info)
-                            .show();
+                    addLimitDialog(CustomRouteMap.this.getString(R.string.markerLimit),googleMap);
                 }
             }
         });
 
     }
 
-//    private void addMarkerDialog(String message){
-//
-//    }
-//    private void mapOnClicklistenerAction implements DialogInterface.OnClickListener{
-//        @Override
-//        public void onClick(DialogInterface dialog, int which) {
-//            Marker marker;
-//    }
+    private void addMarkerWithDialog(String message,GoogleMap map,LatLng latLng){
+        new AlertDialog.Builder(CustomRouteMap.this)         //Alert dialog to add a marker
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Marker marker;
+                        numMarker++;
+                        marker = map.addMarker(new MarkerOptions()                   //marker is added to the map and to arraylist
+                                .position(latLng)
+                                .title("This is marker number " + numMarker + " in my route.")
+                                .draggable(true));
+                        markerList.add(marker);
+                        Log.d(MainActivity.LOGAPP, "A marker was added to the list. The position of the last marker added is "
+                                + markerList.get(markerList.size() - 1).getPosition().toString());
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel, null)
+                .setIcon(android.R.drawable.ic_dialog_map)
+                .show();
+    }
+
+    private void addLimitDialog(String message,GoogleMap map){
+        new AlertDialog.Builder(CustomRouteMap.this)             //can add up to 8 places
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok,null)
+                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {             //map is cleared from markers and user can start over
+                        map.clear();
+                        numMarker=0;
+                        markerList.clear();
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_info)
+                .show();
+    }
 
 }
 

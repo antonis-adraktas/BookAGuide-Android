d798f6f1e88fef5bd1f8c4220fc42297e2548c99
antonis-adraktas <antonis.adraktas@gmail.com>
2020-12-05 14:48:34 +0200

    Added locationManager,permission request,mylocation Button

--- app/src/main/java/com/antonis/bookaguide/CustomRouteMap.java
+++ app/src/main/java/com/antonis/bookaguide/CustomRouteMap.java
@@ -1,15 +1,25 @@
 package com.antonis.bookaguide;
 
+import android.Manifest;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -22,10 +32,15 @@ import java.util.ArrayList;
 
 public class CustomRouteMap extends AppCompatActivity
         implements OnMapReadyCallback {
-    private int numMarker=0;
+    final int REQUEST_CODE = 123;
+    private int numMarker = 0;
     private ArrayList<Marker> markerList;
     private Button finishButton;
     private Button infoButton;
+    LocationManager locationManager;
+    private Location myLocation;
+
+
     // [START_EXCLUDE]
     // [START maps_marker_get_map_async]
     @Override
@@ -38,34 +53,91 @@ public class CustomRouteMap extends AppCompatActivity
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        finishButton=findViewById(R.id.finish);
-        infoButton=findViewById(R.id.info);
+        finishButton = findViewById(R.id.finish);
+        infoButton = findViewById(R.id.info);
         finishButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finishAction();
             }
         });
-        infoButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                provideInfo(CustomRouteMap.this.getString(R.string.provideInfoMap));
-            }
-        });
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE);
+            return;
+        }
+
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void enableMyLocation(GoogleMap map) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            if (map != null) {
+               map.setMyLocationEnabled(true);
+               map.getUiSettings().setMyLocationButtonEnabled(true);
+               locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+               myLocation=locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            }
+        } else {
+            // Permission to access the location is missing. Show rationale and request permission
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE);
+        }
+    }
+
+
+
 
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        markerList=new ArrayList<>();
+        markerList = new ArrayList<>();
+        infoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                provideInfo(CustomRouteMap.this.getString(R.string.provideInfoMap),googleMap);
+            }
+        });
         LatLngBounds atticaBounds = new LatLngBounds(
                 new LatLng(37.882943, 23.657002), // SW bounds
                 new LatLng(38.036468, 23.904720)  // NE bounds
         );
 //        googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(atticaBounds, 10));
-//        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(atticaBounds.getCenter(), 10));
-        googleMap.setLatLngBoundsForCameraTarget(atticaBounds);
-        googleMap.setMinZoomPreference(11);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(atticaBounds.getCenter(), 11));
+        enableMyLocation(googleMap);
+//        googleMap.setLatLngBoundsForCameraTarget(atticaBounds);
+//        googleMap.setMinZoomPreference(11);
+
+
+
+        googleMap.setOnMyLocationClickListener(new GoogleMap.OnMyLocationClickListener() {
+            @Override
+            public void onMyLocationClick(@NonNull Location location) {
+                Toast.makeText(CustomRouteMap.this, "Current location:\n" + location, Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
+
+        googleMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
+            @Override
+            public boolean onMyLocationButtonClick() {
+                Toast.makeText(CustomRouteMap.this, "MyLocation button clicked", Toast.LENGTH_SHORT)
+                        .show();
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                        new LatLng(myLocation.getLatitude(),
+                                myLocation.getLongitude()), 12));
+                return false;
+            }
+        });
+
+        Log.d(MainActivity.LOGAPP,"My location is "+googleMap.isMyLocationEnabled());
+
+
+
 
 
         googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
@@ -124,10 +196,18 @@ public class CustomRouteMap extends AppCompatActivity
         startActivity(intent);
     }
 
-    private void provideInfo(String message){
+    private void provideInfo(String message,GoogleMap map){
         new AlertDialog.Builder(CustomRouteMap.this)
                 .setMessage(message)
                 .setPositiveButton(android.R.string.ok,null)
+                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {             //map is cleared from markers and user can start over
+                        map.clear();
+                        numMarker=0;
+                        markerList.clear();
+                    }
+                })
                 .setIcon(android.R.drawable.ic_dialog_info)
                 .show();
     }

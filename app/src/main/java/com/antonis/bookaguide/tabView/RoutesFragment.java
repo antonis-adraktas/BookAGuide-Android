e7f2880b21c5bf9426b662322d9f438e39b14440
antonis-adraktas <antonis.adraktas@gmail.com>
2020-12-08 16:37:44 +0200

    set onItemClicklistener for RoutesFragment

--- app/src/main/java/com/antonis/bookaguide/tabView/RoutesFragment.java
+++ app/src/main/java/com/antonis/bookaguide/tabView/RoutesFragment.java
@@ -1,19 +1,25 @@
 package com.antonis.bookaguide.tabView;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.antonis.bookaguide.CustomRouteMap;
+import com.antonis.bookaguide.MainActivity;
 import com.antonis.bookaguide.R;
+import com.antonis.bookaguide.data.Routes;
 import com.antonis.bookaguide.listAdapters.RoutesAdapter;
 
 public class RoutesFragment extends Fragment {
@@ -23,6 +29,7 @@ public class RoutesFragment extends Fragment {
 //    private Button sendGuidesButton;
     private RoutesAdapter routeAdapter;
     private ListView routeList;
+    private Routes routeSelected;
 
     public RoutesFragment() {
     }
@@ -45,6 +52,18 @@ public class RoutesFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view =inflater.inflate(R.layout.routes_layout,container,false);
         routeList=view.findViewById(R.id.routesList);
+        routeList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (MainActivity.getSelectedDate()==null){
+                    noDateSelectedDialog();
+                }else{
+                    routeSelected= (Routes) routeList.getItemAtPosition(position);
+                    Log.d(MainActivity.LOGAPP,routeSelected.toString());
+                    Toast.makeText(RoutesFragment.this.getContext(),R.string.routeSelection,Toast.LENGTH_LONG).show();
+                    }
+                }
+        });
         customButton=view.findViewById(R.id.button);
         customButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -85,5 +104,12 @@ public class RoutesFragment extends Fragment {
     public void onStop() {
         super.onStop();
         routeAdapter.cleanup();
+    }
+    private void noDateSelectedDialog(){
+        new AlertDialog.Builder(RoutesFragment.this.getContext())
+                .setMessage("Please select a date for your reservation first")
+                .setPositiveButton(android.R.string.ok,null)
+                .setIcon(android.R.drawable.ic_dialog_info)
+                .show();
     }
 }

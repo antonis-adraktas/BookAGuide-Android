02d26eddf01a5747de62936906bf8ff548f61a1d
antonis-adraktas <antonis.adraktas@gmail.com>
2020-12-09 17:31:10 +0200

    Create Mymarker object with empty constructor

---
+++ app/src/main/java/com/antonis/bookaguide/data/MyMarker.java
@@ -0,0 +1,14 @@
+package com.antonis.bookaguide.data;
+
+public class MyMarker {
+    private LatLng latLng;
+    private String title;
+
+    public MyMarker() {
+    }
+
+    public MyMarker(LatLng latLng, String title) {
+        this.latLng = latLng;
+        this.title = title;
+    }
+}
